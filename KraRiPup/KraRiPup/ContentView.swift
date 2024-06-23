7b2c6962113b60471d178328187b5f6a0435523f
admin <160930169+nicotinelnwza007@users.noreply.github.com>
2024-06-22 21:22:55 -0700

    Update : DatePicker add Calendar

--- KraRiPup/KraRiPup/ContentView.swift
+++ KraRiPup/KraRiPup/ContentView.swift
@@ -1,19 +1,46 @@
-//
-//  ContentView.swift
-//  KraRiPup
-//
-//  Created by admin on 22/6/2567 BE.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var selectedDate = Date()
+
     var body: some View {
-        Text("katangsudlorzaza007")
+        VStack {
+            Spacer()
+
+            Text("")
+                .font(.largeTitle)
+                .padding()
+
+            Spacer()
+
+            DatePicker(
+                "Select a date",
+                selection: $selectedDate,
+                displayedComponents: [.date, .hourAndMinute]
+            )
+            .datePickerStyle(GraphicalDatePickerStyle())
+            .padding()
+
+            Spacer()
+
+            Text("Selected date: \(formattedDate)")
+                .font(.title)
+                .padding()
+
+            Spacer()
+        }
+    }
+
+    var formattedDate: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .short
+        return formatter.string(from: selectedDate)
     }
 }
 
-#Preview{
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
