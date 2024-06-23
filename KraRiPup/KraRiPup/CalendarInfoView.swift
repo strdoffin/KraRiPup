407cf8a8e0dbf76cd2b377ef1cc14efe10b325b4
admin <160930169+nicotinelnwza007@users.noreply.github.com>
2024-06-22 22:22:25 -0700

    seperate a Page

--- KraRiPup/KraRiPup/CalendarInfoView.swift
+++ KraRiPup/KraRiPup/CalendarInfoView.swift
@@ -9,36 +9,54 @@ import SwiftUI
 
 struct ContactInfoView: View {
     @State private var selectedDate = Date()
+    @State private var isActive = false
     var body: some View {
         List{
             Section("Detail"){
-                Text("Name: ")
-                Text("Age: ")
-                Text("School: ")
+                Text("ชื่อคนใช้อ้ะ : ")
             }
             VStack(alignment:.center){
                 VStack {
                     Spacer()
-
-                    Text("")
-                        .font(.largeTitle)
-                        .padding()
+                    VStack{
+                        Text("Wan Nai Wang Bang?")
+                            .font(.largeTitle)
+                        Text("ya bid na i sud")
+                            .font(.title2)
+                    }
 
                     Spacer()
 
+                    // Calendar
                     DatePicker(
                         "Select a date",
                         selection: $selectedDate,
-                        displayedComponents: [.date, .hourAndMinute]
+                        displayedComponents: .date
                     )
                     .datePickerStyle(GraphicalDatePickerStyle())
                     .padding()
 
                     Spacer()
 
-                    Text("Selected date: \(formattedDate)")
-                        .font(.title)
+                    Text("Wanni Wang: \(formattedDate)")
+                        .font(.title2)
                         .padding()
+
+                    Spacer()
+
+                    Button(action: {
+                        printSelectedDate()
+                        self.isActive.toggle()
+                    }) {
+                        Text("ส่งวันที่ว่างจุ้บๆ")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(50)
+                    }
+                    .foregroundColor(isActive ? .gray : .white)
+                    .background(isActive ? Color.gray : Color.blue)
 
                     Spacer()
                 }
@@ -50,6 +68,10 @@ struct ContactInfoView: View {
         formatter.dateStyle = .long
         formatter.timeStyle = .short
         return formatter.string(from: selectedDate)
+    }
+
+    func printSelectedDate() {
+        print("Selected date is \(formattedDate)")
     }
 }
 
