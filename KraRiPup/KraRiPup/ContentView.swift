0672c165a32348b61d1600c2b946152d3e8eebf6
admin <115920771+strdoffin@users.noreply.github.com>
2024-06-22 20:04:17 -0700

    first commit

---
+++ KraRiPup/KraRiPup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KraRiPup
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KraRiPup/KraRiPup/KraRiPupApp.swift
@@ -0,0 +1,17 @@
+//
+//  KraRiPupApp.swift
+//  KraRiPup
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct KraRiPupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
