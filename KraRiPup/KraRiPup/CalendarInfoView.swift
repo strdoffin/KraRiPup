c35eb5b7c1d5d1494aa7d9eba7003df3b7ac14d4
admin <160930169+nicotinelnwza007@users.noreply.github.com>
2024-06-22 22:47:27 -0700

    dawdawdad

--- KraRiPup/KraRiPup/CalendarInfoView.swift
+++ KraRiPup/KraRiPup/CalendarInfoView.swift
@@ -11,56 +11,61 @@ struct ContactInfoView: View {
     @State private var selectedDate = Date()
     @State private var isActive = false
     var body: some View {
-        List{
-            Section("Detail"){
-                Text("ชื่อคนใช้อ้ะ : ")
-            }
-            VStack(alignment:.center){
-                VStack {
-                    Spacer()
-                    VStack{
-                        Text("Wan Nai Wang Bang?")
-                            .font(.largeTitle)
-                        Text("ya bid na i sud")
-                            .font(.title2)
-                    }
+        ZStack{
+                        Color.teal
+                            .ignoresSafeArea()
 
-                    Spacer()
-
-                    // Calendar
-                    DatePicker(
-                        "Select a date",
-                        selection: $selectedDate,
-                        displayedComponents: .date
-                    )
-                    .datePickerStyle(GraphicalDatePickerStyle())
-                    .padding()
-
-                    Spacer()
-
-                    Text("Wanni Wang: \(formattedDate)")
-                        .font(.title2)
-                        .padding()
-
-                    Spacer()
-
-                    Button(action: {
-                        printSelectedDate()
-                        self.isActive.toggle()
-                    }) {
-                        Text("ส่งวันที่ว่างจุ้บๆ")
-                            .font(.title)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
-                            .cornerRadius(50)
-                    }
-                    .foregroundColor(isActive ? .gray : .white)
-                    .background(isActive ? Color.gray : Color.blue)
-
-                    Spacer()
+            List{
+                Section("Detail"){
+                    Text("ชื่อคนใช้อ้ะ : ")
                 }
-            }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                VStack(alignment:.center){
+                    VStack {
+                        Spacer()
+                        VStack{
+                            Text("Wan Nai Wang Bang?")
+                                .font(.largeTitle)
+                            Text("ya bid na i sud")
+                                .font(.title2)
+                        }
+                        
+                        Spacer()
+                        
+                        // Calendar
+                        DatePicker(
+                            "Select a date",
+                            selection: $selectedDate,
+                            displayedComponents: .date
+                        )
+                        .datePickerStyle(GraphicalDatePickerStyle())
+                        .padding()
+                        
+                        Spacer()
+                        
+                        Text("Wanni Wang: \(formattedDate)")
+                            .font(.title2)
+                            .padding()
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            printSelectedDate()
+                            self.isActive.toggle()
+                        }) {
+                            Text("ส่งวันที่ว่างจุ้บๆ")
+                                .font(.title)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(50)
+                        }
+                        .foregroundColor(isActive ? .gray : .white)
+                        .background(isActive ? Color.gray : Color.blue)
+                        
+                        Spacer()
+                    }
+                }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            }
         }
     }
     var formattedDate: String {
